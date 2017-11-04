2ca28be1e022194e4992f39b3cf6e697f42974ad
Akarshan Gandotra <akarshan.gandotra@gmail.com>
2017-11-05 05:11:38 +0530

    solves bug
    multiple file processing now available

--- first.py
+++ first.py
@@ -6,13 +6,11 @@ pd.set_option('display.max_rows', 500)
 file_list = ['/Users/akarshan/Desktop/LTU_4_MUMBAI.xlsx', '/Users/akarshan/Desktop/Mumbai_2.xlsx',
              '/Users/akarshan/Desktop/Mumbai_4.xlsx']
 
-final_dict = []
-final_total_values = []
-final_total_places = []
 final_list = []
 
 
 def filter_1(df):
+    # companies name
     dict_company = OrderedDict()
     for i in range(df.shape[0]):
         a = str(df.iloc[i, 0])
@@ -35,7 +33,7 @@ def filter_2(df, dict_company):
                 temp_li.append(df.iloc[j, z])
             li_total_values.append(temp_li)
         if a == 'Payment for the Month' or a == 'Amount reduced' or a == 'Addition During the Month':
-            dict_company.update({a: [j, 2]})
+            dict_company.update({a: j})
     return li_total_places, li_total_values, dict_company
 
 
@@ -52,51 +50,32 @@ def function(file_list):
     li_total_places_alpha, li_total_values_alpha, dict_company_alpha = filter_2(df_alpha, dict_company_alpha)
     li_total_places_beta, li_total_values_beta, dict_company_beta = filter_2(df_beta, dict_company_beta)
 
-    print dict_company_alpha
-    print dict_company_beta
-
     for key in dict_company_alpha:
         if key in dict_company_beta:
-            if key == 'Payment for the Month':
+            if "Due" in str(key):
+                #df_alpha_new = df_alpha.iloc[dict_company_alpha[key]:]
+                #df_beta_new = df_beta.iloc[dict_company_alpha[key]:]
                 break
+
             else:
-                df_alpha_new = df_alpha.iloc[dict_company_alpha[key]:li_total_places_alpha[dict_company_alpha.keys().index(key)]]
-                df_beta_new = df_beta.iloc[dict_company_beta[key]:li_total_places_beta[dict_company_beta.keys().index(key)]]
-                print df_beta_new
-                print "------"
-                final_list.append(df_alpha_new)
-                final_list.append(df_beta_new)
+                df_alpha_new = df_alpha.iloc[
+                               dict_company_alpha[key]:li_total_places_alpha[dict_company_alpha.keys().index(key)]]
+                df_beta_new = df_beta.iloc[
+                              dict_company_beta[key] + 1:li_total_places_beta[dict_company_beta.keys().index(key)] + 1]
+
+            final_list.append(df_alpha_new)
+            final_list.append(df_beta_new)
+
     result = pd.concat(final_list, ignore_index=True)
     writer = pd.ExcelWriter('/Users/akarshan/Desktop/final.xlsx')
     df_gamma = pd.DataFrame(result)
-    df_gamma.to_excel(writer,'Feb. sheet 8')
+    df_gamma.to_excel(writer, 'Feb. sheet 8')
     writer.save()
 
 
-
 for i in range(len(file_list) - 1):
-    function([file_list[i], file_list[i + 1]])
-    file_list.remove(file_list[i])
-    file_list.remove(file_list[i + 1])
-    break
-
-
-
-
-
-
-
-
-
-    # dfalpha = df.iloc[dict_company.values()[0][0]:li_total[0]]
-
-
-    # frames = [df_beta, df_alpha]
-
-
-    # result = pd.concat(frames, ignore_index=True)
-    # print result
-    # writer = pd.ExcelWriter('/Users/akarshan/Desktop/finally.xlsx')
-    # df3 = pd.DataFrame(result)
-    # df3.to_excel(writer,'Sheet')
-    # writer.save()
+    if i == 0:
+        function([file_list[i], file_list[i + 1]])
+    else:
+        final_list = []
+        function(['/Users/akarshan/Desktop/final.xlsx',file_list[i + 1]])
