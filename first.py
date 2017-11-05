0eb7c1ee07c5b67b6f2e0d75d1a74e489eddf1c2
Akarshan Gandotra <akarshan.gandotra@gmail.com>
2017-11-05 13:09:17 +0530

    Update first.py

--- first.py
+++ first.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import regex as re
 from collections import OrderedDict
+import math
+
 
 import sys
 import os
@@ -16,9 +18,8 @@ pd.set_option('display.max_rows', 500)
 
 file_list = [x.strip() for x in open("path.txt").readlines()]
 
-#file_list = ['/Users/akarshan/Desktop/LTU_4_MUMBAI.xlsx', '/Users/akarshan/Desktop/Mumbai_2.xlsx','/Users/akarshan/Desktop/Mumbai_4.xlsx']
-
 final_list = []
+
 
 
 def filter_1(df):
@@ -53,8 +54,10 @@ def function(file_list):
     raw_data = pd.read_excel(file_list[0])
     df_alpha = pd.DataFrame(raw_data)
 
+
     raw_data = pd.read_excel(file_list[1])
     df_beta = pd.DataFrame(raw_data)
+
 
     dict_company_alpha = filter_1(df_alpha)
     dict_company_beta = filter_1(df_beta)
@@ -77,12 +80,12 @@ def function(file_list):
 
             final_list.append(df_alpha_new)
             final_list.append(df_beta_new)
-
-    result = pd.concat(final_list, ignore_index=True)
-    writer = pd.ExcelWriter('Nextcloud/'+sys.argv[1]+'/final.xlsx')
-    df_gamma = pd.DataFrame(result)
-    df_gamma.to_excel(writer, 'Feb. sheet 8')
-    writer.save()
+    if final_list:
+        result = pd.concat(final_list, ignore_index=True)
+        writer = pd.ExcelWriter('Nextcloud/' + sys.argv[1] + '/final.xlsx')
+        df_gamma = pd.DataFrame(result)
+        df_gamma.to_excel(writer, 'Feb. sheet 8')
+        writer.save()
 
 
 for i in range(len(file_list) - 1):
@@ -90,4 +93,56 @@ for i in range(len(file_list) - 1):
         function([file_list[i], file_list[i + 1]])
     else:
         final_list = []
-        function(['Nextcloud/'+sys.argv[1]+'/final.xlsx',file_list[i + 1]])
+        function(['Nextcloud/' + sys.argv[1] + '/final.xlsx',file_list[i + 1]])
+
+data = pd.read_excel('Nextcloud/' + sys.argv[1] + '/final.xlsx')
+df_gamma = pd.DataFrame(data)
+
+
+list_of_unwanted_data = []
+for i in range(df_gamma.shape[0]):
+    a = str(df_gamma.iloc[i, 0])
+    if a != 'nan':
+        if a == 'Tin No.':
+            list_of_unwanted_data.append(i)
+
+for j in list_of_unwanted_data:
+    df_gamma = df_gamma.drop(j, axis = 0)
+
+df_gamma = df_gamma.rename(columns={'29.02.2016':'Tin No.', 'Unnamed: 1':'DBA Entry No /Page No.', 'Unnamed: 2':'Name of the dealer', 'Unnamed: 3':'Period', 'Unnamed: 4':'VAT Tax', 'Unnamed: 5':'Int 30(2)&(3)'
+                                    ,'Unnamed: 6':'Penalty', 'Unnamed: 7':'NO', 'Unnamed: 8':'Total Amount (VAT)', 'Unnamed: 9':'CST Tax',
+                                    'Unnamed: 10':'Int30(2)&(3)', 'Unnamed: 11':'penalty', 'Unnamed: 12':'NO', 'Unnamed: 13':'Total Amount (CST)','Unnamed: 14':'remark' })
+
+
+
+a = filter_2(df_gamma,filter_1(df_gamma))
+print a[2]
+print a[0]
+wow = list(df_gamma.columns.values)
+
+
+for y in range(4,13):
+    sum = 0
+    l = 0
+    for keys in a[2]:
+        for i in range(a[2][keys] + 1, a[0][l]):
+            if not math.isnan(float(df_gamma.iloc[i, y])):
+                sum += float(df_gamma.iloc[i, y])
+        print sum
+
+        df_gamma.ix[i + 4, wow[y]] = sum
+
+        #y, a[0][l]
+        sum = 0
+        l += 1
+
+
+
+#print df_g amma_new
+
+
+
+writer = pd.ExcelWriter('Nextcloud/' + sys.argv[1] + '/final.xlsx')
+df_gamma.to_excel(writer, 'Feb. sheet 8')
+df_gamma.to_excel(writer, 'VAT')
+writer.save()
